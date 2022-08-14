99ba6b6ec66f9757fb4769ebac7b4f481d57e997
Nacer <41012118@parisnanterre.fr>
2022-08-15 00:57:27 +0200

    commit à jour

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -1,7 +1,16 @@
 import { NgModule } from '@angular/core';
 import { RouterModule, Routes } from '@angular/router';
+import {AboutComponent} from "./pages/about/about.component";
+import {ContactComponent} from "./pages/contact/contact.component";
+import {ExpertiseComponent} from "./pages/expertise/expertise.component";
+import {PortfolioComponent} from "./pages/portfolio/portfolio.component";
 
-const routes: Routes = [];
+const routes: Routes = [
+  {path:'qui_suis_je',component:AboutComponent},
+  {path:'expertise',component:ExpertiseComponent},
+  {path:'portfolio',component:PortfolioComponent},
+  {path:'contact',component:ContactComponent},
+];
 
 @NgModule({
   imports: [RouterModule.forRoot(routes)],
